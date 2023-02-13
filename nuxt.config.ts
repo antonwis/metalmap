b8b86d2ae16bb4d20fb4cd66070a3849c0d3d026
antonwis <anton.wiskari@metropolia.fi>
2023-02-13 21:07:36 +0200

    metadata

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,11 +4,18 @@ export default defineNuxtConfig({
     nitro: {
         plugins: ["~/server/index.ts"],        
     },
-
     runtimeConfig: {
         dbSocket: process.env.API_SOCKET,
         dbUser: process.env.API_USER,
         dbPassword: process.env.API_PW,
         dbDatabaseName: process.env.API_DB
+    },
+    app: {
+        head: {
+            title: "Black Metal World Map",
+            meta: [{
+                name: "description", content: "Browse metal bands from all around the world"
+            }]
+        }
     }
 })
